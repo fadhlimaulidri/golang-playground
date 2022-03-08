423e1ecf8c9b29dbd58ab441e54af1936a907208
Fadhli Maulidri <fadhli@bukalapak.com>
2022-03-08 12:21:03 +0700

    list endpoint per environment

--- main.go
+++ main.go
@@ -7,13 +7,22 @@ import (
 	"log"
 	"net/http"
 	"os"
+
 	"github.com/subosito/gotenv"
 )
 
 type Eenv struct {
 	Base_url string
-	Endpoint string
+	Endpoint []string
 	Port     string
+}
+
+type Request struct {
+	// The host on which the URL is sought.
+	// Per RFC 2616, this is either the value of the Host: header
+	// or the host name given in the URL itself.
+	// It may be of the form "host:port".
+	Host string
 }
 
 type Article struct {
@@ -28,22 +37,72 @@ func init() {
 	gotenv.Load()
 }
 
+func Find(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
+
 func allArticles(w http.ResponseWriter, r *http.Request) {
 	articles := Articles{
 		Article{Title: "Test Title", Desc: "Test Description", Content: "Hello World"},
 	}
 	fmt.Println("endpoint hit all article")
+	fmt.Printf("Req: %s %s\n", r.Host, r.URL.Path)
+
 	json.NewEncoder(w).Encode(articles)
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the HomePage!")
-	fmt.Println("Endpoint Hit: homePage")
+	configfile := "config/env/" + os.Getenv("ENVIRONTMENT") + ".json"
+	envlocal, err := ioutil.ReadFile(configfile)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	var env Eenv
+	json.Unmarshal([]byte(envlocal), &env)
+	found := Find(env.Endpoint, "/")
+
+	if !found {
+		fmt.Println("Page not found")
+	}
+
+	if found {
+		fmt.Fprintf(w, "Welcome to the HomePage!")
+	} else {
+		w.WriteHeader(404)
+	}
 }
 
-func handleRequests() {
-	// envlocal := `{"base_url": "localhost","endpoint": "bla","port": ":8080"}`
-	envlocal, err := ioutil.ReadFile("config/env/local.json")
+func stagingPage(w http.ResponseWriter, r *http.Request) {
+	configfile := "config/env/" + os.Getenv("ENVIRONTMENT") + ".json"
+	envlocal, err := ioutil.ReadFile(configfile)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	var env Eenv
+	json.Unmarshal([]byte(envlocal), &env)
+
+	found := Find(env.Endpoint, "/staging")
+	if !found {
+		fmt.Println("Page not found")
+	}
+
+	if found {
+		fmt.Fprintf(w, "Welcome to the HomePagea!")
+	} else {
+		w.WriteHeader(404)
+	}
+}
+
+func main() {
+	configfile := "config/env/" + os.Getenv("ENVIRONTMENT") + ".json"
+	envlocal, err := ioutil.ReadFile(configfile)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -54,10 +113,6 @@ func handleRequests() {
 
 	http.HandleFunc("/", homePage)
 	http.HandleFunc("/articles", allArticles)
+	http.HandleFunc("/staging", stagingPage)
 	log.Fatal(http.ListenAndServe(env.Base_url+":"+env.Port, nil))
-}
-
-func main() {
-	log.Println(os.Getenv("APP_ID"))
-	handleRequests()
 }
